6ff2a935e23ee8c76dee2d7311ec16863bc89ae9
rubenfernandezpardo <r.fp09@hotmail.com>
2020-01-27 20:18:07 +0100

    27/01/20

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
-// window.onload = function() {
-//     // // CUANDO USAMOS EL SCROLL EN NUESTRA PAGINA, SE EJECUTA LA FUNCION WINDOW SCROLL
-//     window.onscroll = function() {myFunction()};
-//
-//     var navbar = document.getElementById("navbar");
-//
-//      // obtenemos la posición de desplazamiento de la barra de navegación
-//      var sticky = navbar.offsetTop;
-//
-//     // añadimos la clase "sticky" cuando se mueve el scroll. Se borra el "sticky" tu dejas de usarlo el scroll
-//     function myFunction() {
-//       if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("sticky")
-//       } else {
-//         navbar.classList.remove("sticky");
-//       }
-//     }
-// }
+// AQUÍ HACEMOS CON JS QUE EL NAVBAR CUANDO BAJEMOS (SCROLL) EL MENÚ SEA PEGUE ARRIBA
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+
+});
+// __________________________________________________________________
